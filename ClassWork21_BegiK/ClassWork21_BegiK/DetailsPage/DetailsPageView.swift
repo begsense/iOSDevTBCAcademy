f7aa9a59130d187a51b2399d28243288381c10d6
begsense <begsense@gmail.com>
2024-04-25 19:55:46 +0400

    modify again ClassWork21

--- ClassWork21_BegiK/ClassWork21_BegiK/DetailsPage/DetailsPageView.swift
+++ ClassWork21_BegiK/ClassWork21_BegiK/DetailsPage/DetailsPageView.swift
@@ -7,24 +7,3 @@
 
 import UIKit
 
-class DetailsPageViewModel {
-    var countryDetails: CountryDetails?
-
-    // Fetch country details from a service or local storage
-    func fetchCountryDetails() {
-        // Implement the logic to fetch details
-    }
-
-    // Handle user actions like opening a map link
-    func openStreetMap() {
-        openURLInSafari("https://www.openstreetmap.org/")
-    }
-
-    func openGoogleMap() {
-        openURLInSafari("https://www.google.com/maps")
-    }
-
-    private func openURLInSafari(_ urlString: String) {
-        // Implement opening URL
-    }
-}
